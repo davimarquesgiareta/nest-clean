84d6a82e953fcbf8f0257aaa232f2e2b20351c8c
Davi Marques <davimgcb@gmail.com>
2025-05-20 13:47:24 -0300

    feat: creating validation pipe

--- nest-clean/src/controllers/create-account.controller.ts
+++ nest-clean/src/controllers/create-account.controller.ts
@@ -4,9 +4,20 @@ import {
   Controller,
   HttpCode,
   Post,
+  UsePipes,
 } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { hash } from 'bcryptjs';
+import { z } from 'zod';
+import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe';
+
+const createAccountBodySchema = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  password: z.string(),
+});
+
+type CreateAccountBodySchema = z.infer<typeof createAccountBodySchema>;
 
 @Controller('/accounts')
 export class CreateAccountController {
@@ -14,13 +25,12 @@ export class CreateAccountController {
 
   @Post()
   @HttpCode(201)
-  async handle(@Body() body: any) {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  @UsePipes(new ZodValidationPipe(createAccountBodySchema))
+  async handle(@Body() body: CreateAccountBodySchema) {
     const { name, email, password } = body;
 
     const userWithSameEmail = await this.prisma.user.findUnique({
       where: {
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
         email,
       },
     });
---
+++ nest-clean/src/pipes/zod-validation-pipe.ts
@@ -0,0 +1,26 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { PipeTransform, BadRequestException } from '@nestjs/common';
+import { ZodError, ZodSchema } from 'zod';
+import { fromZodError } from 'zod-validation-error';
+
+export class ZodValidationPipe implements PipeTransform {
+  constructor(private schema: ZodSchema) {}
+
+  transform(value: unknown) {
+    try {
+      this.schema.parse(value);
+    } catch (error) {
+      if (error instanceof ZodError) {
+        throw new BadRequestException({
+          message: 'Validation failed',
+          statusCode: 400,
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+          errors: fromZodError(error),
+        });
+      }
+
+      throw new BadRequestException('Validation failed');
+    }
+    return value;
+  }
+}

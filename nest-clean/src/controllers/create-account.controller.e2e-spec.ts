3ab7f1d1b796f2cba4bc157eaef2ff71d4b61ac3
Davi Marques <davimgcb@gmail.com>
2025-05-22 11:35:13 -0300

    feat: vitest with SWC config

---
+++ nest-clean/src/controllers/create-account.controller.e2e-spec.ts
@@ -0,0 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+test('1 plus 1', () => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+  expect(1 + 1).toBe(2);
+});
